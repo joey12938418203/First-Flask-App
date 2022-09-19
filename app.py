cfee2f28700a26a6639d3a93c506135ce5910a36
joey12938418203 <jalewis9000@gmail.com>
2022-09-18 22:18:34 -0700

    Changing to postgres

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 # from flask_jwt import JWT
@@ -17,7 +19,7 @@ from db import db
 
 #API Itself
 app=Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='dark_secret'
 api=Api(app)
